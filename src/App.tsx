a7636078a85754c58b5722efc144dbcce9bc4700
박흥준 Rock <rockpark.bcn@gmail.com>
2019-11-26 17:25:33 +0900

    Update get object logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useCallback } from "react";
+import React, { useRef, useCallback, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import AWS from "./aws.config";
@@ -6,8 +6,9 @@ import uuidv4 from "uuid/v4";
 
 const App: React.FC = () => {
   const inputRef = useRef(null);
+  const [path, setPath] = useState("data:image/png;base64,0909090");
   const _upload = useCallback(async e => {
-    await new Promise((res, rej) => {
+    const result: any = await new Promise((res, rej) => {
       e.preventDefault();
       const file = inputRef.current.files[0];
       new AWS.S3.ManagedUpload({
@@ -19,16 +20,30 @@ const App: React.FC = () => {
       }).send((err, data) => {
         if (err) {
           rej(err);
+          console.log(err);
           return;
         }
-        console.log(data);
         res(data);
       });
     });
+
+    console.log(result);
+    const s3 = new AWS.S3();
+    s3.getObject({ Bucket: "heung.rocks", Key: result.key }, (err, data) => {
+      console.log(data);
+      setPath(URL.createObjectURL(new (Blob as any)([data.Body])));
+    });
   }, []);
+
+  // useEffect(() => {
+
+  //   return () => {
+  //   };
+  // }, [path])
 
   return (
     <div className="App">
+      <img src={path} onError={() => {}} />
       <form onSubmit={_upload}>
         <input type={"file"} ref={inputRef} />
         <input type={"submit"} value={"제출"} />
